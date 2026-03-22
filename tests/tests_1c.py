3666ecf9da8c5a4b43d93472d1d5ed94f95aa45c
33ace <bmcben7272@gmail.com>
2026-03-22 08:06:18 -0400

    Corrected Labs C and D Errors/Added Tests

---
+++ tests/tests_1c.py
@@ -0,0 +1,8 @@
+from lab_1c import max_subarray_sum
+
+def test_simple_list():
+    numbers [1, 2, 3]
+    assert max_subarray_sum(my_list) == 6
+
+def test_with_negatives():
+    assert max_subarray_sum([-5, -2, -10]) == 6
---
+++ tests/tests_1d.py
@@ -0,0 +1,7 @@
+from lab_1d import two_sum
+
+def test_d_basic():
+    assert two_sum([3, 1, 4], 7) == [0, 2]
+
+def test_d_big_numbers():
+    assert two_sum([10, 20, 30, 40], 70) == [2, 3]
