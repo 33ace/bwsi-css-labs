9bb3ef9181d3f1aa63746818aee1b62a4faa4200
33ace <bmcben7272@gmail.com>
2026-03-22 10:54:10 -0400

    Fixed typos in test_1c.py

--- tests/test_1c.py
+++ tests/test_1c.py
@@ -1,7 +1,7 @@
 from labs.lab_1.lab_1c import max_subarray_sum
 
 def test_simple_list():
-    numbers [1, 2, 3]
+    my_list = [1, 2, 3]
     assert max_subarray_sum(my_list) == 6
 
 def test_with_negatives():
